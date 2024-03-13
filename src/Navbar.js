ed6d0b419c49dfa155f8db8e1f6b75950fed3479
gribelUS <tiagobreunig1@gmail.com>
2024-03-13 02:45:01 -0400

    .

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,16 +7,24 @@ function Navbar() {
             <a class="navbar-brand" href="#">Amplify App</a>
         </div>
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+                </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Page 1</a>
+                </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Page 2</a>
+                </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Page 3</a>
+                </li>
         </ul>
         <form class="navbar-form navbar-left" action="/action_page.php">
             <div class="form-group">
                 <input type="text" class="form-control" placeholder="Search"></input>
             </div>
-            <button type="submit" class="btn btn-default">Submit</button>
+            <button type="submit" class="btn btn-outline-success my-2 my-sm-0">Submit</button>
         </form>
     </div>
 </nav>
