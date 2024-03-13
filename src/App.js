a7c43a2c2f9c3f3b3a2e279229a83abab2d8b749
gribelUS <tiagobreunig1@gmail.com>
2024-03-13 01:05:15 -0400

    Revert "Upp"
    This reverts commit efe27b207f40fd2acdbb69990e0104d096725237.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar.js';
-import Card from './Card.js';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800394502</p>
       <p>Hi, I am Tiago.</p>
-      <Card />
-      <Card />
-      <Card />
     </div>
   );
 }
