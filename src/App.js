6f47d55587197dfa8ef08e30e8367664855ff198
gribelUS <tiagobreunig1@gmail.com>
2024-03-13 01:58:34 -0400

    c

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
       <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800394502</p>
       <p>Hi, I am Tiago.</p>
+      </div>
+      <Card />
     </div>
   );
 }
