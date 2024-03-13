600847a509f195ef5d54da12f03030ae12cba278
gribelUS <tiagobreunig1@gmail.com>
2024-03-13 03:01:09 -0400

    fix

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,28 @@
 
 function Navbar() {
     return (
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Amplify App</a>
-        </div>
-        <ul class="nav navbar-nav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<a class="navbar-brand" href="#">Amplify App</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">  
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home</a>
-                </li>
+            </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Page 1</a>
-                </li>
+                <a class="nav-link" href="#">About</a>
+            </li>
             <li class="nav-item active">
-                <a class="nav-link" href="#">Page 2</a>
-                </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Page 3</a>
-                </li>
+                <a class="nav-link" href="#">Contact</a>
+            </li>
         </ul>
-        <form class="navbar-form navbar-left" action="/action_page.php">
-            <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search"></input>
-            </div>
+        </div>
+        <form class="form-inline my-2 my-lg-0">
+                <input type="text" class="form-control mr-sm-2" placeholder="Search" aria-label="Search"></input>
+        </form>        
             <button type="submit" class="btn btn-outline-success my-2 my-sm-0">Submit</button>
-        </form>
-    </div>
 </nav>
     );
 }
