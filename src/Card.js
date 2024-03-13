7d181c7e2169617fe40205fe023941c6076ca8b7
gribelUS <tiagobreunig1@gmail.com>
2024-03-13 02:27:13 -0400

    .

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,36 @@
-
-function Card () {
-    return(
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Card Title</h5>
-                    <p class="cart-text">Some text inside of the card</p>
-                </div>
-            </div>
+function Card() {
+    return (
+        <div class = "card-group">
+        <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'blue'}}>
+        <div className="card-body" style={{}}>
+          <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card 01</h5>
+          <p className="card-text" style={{color:'white'}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
         </div>
+      </div>
+      <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'red'}}>
+      <div className="card-body" style={{}}>
+        <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card 02</h5>
+        <p className="card-text" style={{color:'white'}}>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
     </div>
+
+    <div className="card" style={{ width: '18rem', margin: '5px', backgroundColor:'green'}}>
+      <div className="card-body" style={{}}>
+        <h5 className="card-title" class="card-container d-flex justify-content-center" style={{color:'white'}}>Card 03</h5>
+        <p className="card-text" style={{color:'white'}}>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+    </div>
+      
     );
 }
 
